bddf0dd1093ce09d8dce519e03ca481cacf90c16
a-s-wijaya <adrian.s.wijaya@upi.edu>
2023-05-03 11:15:48 +0700

    Relation fix

--- models/socialmedia.js
+++ models/socialmedia.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class SocialMedia extends Model {
     /**
@@ -10,34 +8,35 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.User);
     }
   }
-  SocialMedia.init({
-    name: { 
-      type: DataTypes.STRING, 
-      allowNull: false,
-      validate: {
-        notEmpty: {
-          msg: "Name cannot be empty"
-        }
-
-      }
+  SocialMedia.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Name cannot be empty",
+          },
+        },
+      },
+      social_media_url: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          isUrl: true,
+          notEmpty: {
+            msg: "Social URL cannot be empty",
+          },
+        },
+      },
     },
-    social_media_url: { 
-      type: DataTypes.TEXT, 
-      allowNull: false,
-      validate: {
-        isUrl:true,
-        notEmpty: {
-          msg: "Social URL cannot be empty"
-        }
-
-      }
-    },
-  }, {
-    sequelize,
-    modelName: 'SocialMedia',
-  });
+    {
+      sequelize,
+      modelName: "SocialMedia",
+    }
+  );
   return SocialMedia;
 };
